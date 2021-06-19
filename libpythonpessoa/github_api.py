74867b2168e6ee9cba6969690a583ffd192c35ba
pessoasnil <Metroide171190y@>
2021-06-18 22:46:26 -0300

    Instalando outras bibliotecas e testando projetos de terceiros

---
+++ libpythonpessoa/github_api.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuario no Github
+    :param usuario: str com nome de usuário no github
+    :return: str com link do avatar
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+if __name__ == '__main__':
+    print(buscar_avatar('pessoasnil'))
+
+
+
